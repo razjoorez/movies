cb21106b7a6ea020119ad09e52aa909fa918791d
Reza Razjoo <reza.razjoo@yahoo.com>
2024-04-16 17:21:17 +0100

    first commit

---
+++ src/test/java/dev/razjoo/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.razjoo.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
